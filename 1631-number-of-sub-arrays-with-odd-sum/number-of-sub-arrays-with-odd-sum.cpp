f484bbeab1d8c8ae03268b61ff67de4bda296403
Yaseen Abdulghany <62149387+yaseenabdulghany@users.noreply.github.com>
2025-02-25 21:00:25 +0200

    Time: 4 ms (62.15%) | Memory: 112 MB (55.17%) - LeetSync

---
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,17 @@
+#define mod 1000000007
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int result = 0, sum = 0;
+        vector<int> freq (2, 0);
+        freq[0]++;  
+        
+        for(int i = 0; i < arr.size(); i++) {
+            sum += arr[i];
+            result += freq[!(sum % 2)];
+            freq[sum % 2]++;           
+            result %= mod;
+        }
+        return result;
+    }
+};
